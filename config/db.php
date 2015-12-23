1fb47ca1bb6d2a8fd14137bc45799166cd2f48a4
shumer596 <shumerok@gmail.com>
2015-12-23 13:55:25 +0200

    'init'

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=site.loc',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
