a543a29e743fbc85eb6e92bcbad9ffb803c1d842
shumer596 <shumerok@gmail.com>
2016-01-14 12:14:56 +0200

    add ActiveForms to register form

--- views/site/register.php
+++ views/site/register.php
@@ -11,7 +11,7 @@ use app\models\City;
 /* @var $form ActiveForm */
 ?>
 <div class="site-register">
-
+    <?php $form = ActiveForm::begin(); ?>
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'status')->dropDownList([
@@ -32,10 +32,6 @@ use app\models\City;
     <div><?= $form->field($model, 'city')->widget(
             AutoComplete::className(), [
             'clientOptions' => [
-                //                'source' => City::find()
-                //                    ->select(['city_id as value', 'CONCAT(name,\', \', region_id ) as label'])
-                //                    ->asArray()
-                //                    ->all(),
                 'source' => City::find()
                     ->select(['city_id as value', 'CONCAT(city.name,\', \', region.name ) as label'])
                     ->innerJoin('region', 'city.region_id = region.region_id')
@@ -57,11 +53,12 @@ use app\models\City;
     <div><?= $form->field($model, 'skype') ?></div>
     <div><?= $form->field($model, 'email') ?></div>
     <div><?= $form->field($model, 'password') ?></div>
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Submit'), ['class' => 'btn btn-primary']) ?>
+    </div>
     <?php ActiveForm::end(); ?>
+
     <?php $form = ActiveForm::begin(['id' => 'Legal_form']); ?>
-    <div class="show2"><?= $form->field($model, 'INN') ?></div>
-    <div class="show2"><?= $form->field($model, 'address') ?></div>
-    <div class="show2"><?= $form->field($model, 'company') ?></div>
     <div class="show2"><?= $form->field($model, 'type_ownership')->dropDownList([
             User::PLC => Yii::t('app', 'PLC'),
             User::LTD => Yii::t('app', 'Ltd'),
@@ -73,11 +70,47 @@ use app\models\City;
             User::IC => Yii::t('app', 'IC'),
             User::LP => Yii::t('app', 'LP'),
         ], ['prompt' => Yii::t('app', 'Select the legal form')]) ?></div>
+    <div class="show2"><?= $form->field($model, 'company') ?></div>
+    <div> <?= $form->field($model, 'activity')->dropDownList([
+            User::CARRIER => Yii::t('app', 'Carrier(company)'),
+            User::FORWARDER => Yii::t('app', 'Forwarder(company)'),
+            User::SNIPER => Yii::t('app', 'Shipper(company)'),
+            User::DISPATCHER => Yii::t('app', 'Dispatcher(company)'),
+            User::FORWARDER_CARRIER => Yii::t('app', 'Forwarder-carrier(company)'),
+            User::SNIPER_CARRIER => Yii::t('app', 'Shipper-carrier(company)'),
+            User::INSURANCE_AGENT => Yii::t('app', 'Insurance company'),
+        ], ['prompt' => Yii::t('app', 'Select activity')]) ?></div>
+    <div class="show2"><?= $form->field($model, 'INN') ?></div>
+    <div><?= $form->field($model, 'city')->widget(
+            AutoComplete::className(), [
+            'clientOptions' => [
+                'source' => City::find()
+                    ->select(['city_id as value', 'CONCAT(city.name,\', \', region.name ) as label'])
+                    ->innerJoin('region', 'city.region_id = region.region_id')
+                    ->orderBy("city_id asc")
+                    ->asArray()
+                    ->all()
+            ],
+            'options' => [
+                'class' => 'form-control',
+                'placeholder' => Yii::t('app', 'Start typing the name')
+            ]
+        ]); ?>
+    </div>
+    <div class="show2"><?= $form->field($model, 'address') ?></div>
+    <div><?= $form->field($model, 'surName') ?></div>
+    <div><?= $form->field($model, 'firstName') ?></div>
+    <div><?= $form->field($model, 'lastName') ?></div>
+    <div><?= $form->field($model, 'number') ?></div>
+    <div><?= $form->field($model, 'skype') ?></div>
     <div class="show2 hide2"><?= $form->field($model, 'site')->textInput(['value' => 'http://']) ?></div>
+    <div><?= $form->field($model, 'email') ?></div>
+    <div><?= $form->field($model, 'password') ?></div>
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Submit'), ['class' => 'btn btn-primary']) ?>
     </div>
 
+    <?php ActiveForm::end(); ?>
 
 </div><!-- site-register -->
